c5ba4132e00c3f1f66e044d83311a286ab113b84
Discover- <amort11@hotmail.com>
2013-12-22 13:30:48 +0100

    Add the solution, a blank MainForm layout file, a Database class, WorldDatabase class and the MySql library

---
+++ Spell-Editor/Database.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using System.Data;
+
+namespace Spell_Editor
+{
+    public class Database<Connection, StrBuilder, Parameter, Command, Transaction>
+        where Connection : DbConnection
+        where Command : DbCommand
+        where Transaction : DbTransaction
+    {
+        public StrBuilder connectionString { get; set; }
+
+        public bool CanConnectToDatabase(MySqlConnectionStringBuilder _connectionString)
+        {
+            bool exceptionOccurred = false;
+
+            try
+            {
+                //! Close the connection again since this is just a try-connection function. We actually connect
+                //! when the mainform is opened (this happens automatically because we use 'using').
+                using (MySqlConnection connection = new MySqlConnection(_connectionString.ToString()))
+                    connection.Open();
+            }
+            catch (MySqlException)
+            {
+                exceptionOccurred = true;
+            }
+
+            return !exceptionOccurred;
+        }
+
+        public async Task<bool> ExecuteNonQuery(string nonQuery, params Parameter[] parameters)
+        {
+            bool exceptionOccurred = false;
+
+            await Task.Run(() =>
+            {
+                using (Connection conn = (Connection)Activator.CreateInstance(typeof(Connection), connectionString.ToString()))
+                {
+                    conn.Open();
+
+                    using (Command cmd = (Command)Activator.CreateInstance(typeof(Command), nonQuery, conn))
+                    {
+                        try
+                        {
+                            foreach (var param in parameters)
+                                cmd.Parameters.Add(param);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                        catch (Exception)
+                        {
+                            exceptionOccurred = true;
+                        }
+                    }
+
+                    conn.Close();
+                }
+            });
+
+            return !exceptionOccurred;
+        }
+
+        public Task<DataTable> ExecuteQuery(string query, params Parameter[] parameters)
+        {
+            return ExecuteQueryWithCancellation(new CancellationToken(), query, parameters);
+        }
+
+        public async Task<DataTable> ExecuteQueryWithCancellation(CancellationToken token, string query, params Parameter[] parameters)
+        {
+            return await Task.Run(async () =>
+            {
+                using (Connection conn = (Connection)Activator.CreateInstance(typeof(Connection), connectionString.ToString()))
+                {
+                    conn.Open();
+
+                    using (Command cmd = (Command)Activator.CreateInstance(typeof(Command), query, conn))
+                    {
+                        foreach (var param in parameters)
+                            cmd.Parameters.Add(param);
+
+                        try
+                        {
+                            var reader = await cmd.ExecuteReaderAsync(token);
+
+                            if (token.IsCancellationRequested)
+                                token.ThrowIfCancellationRequested();
+
+                            var dt = new DataTable();
+                            dt.Load(reader);
+                            conn.Close();
+                            return dt;
+                        }
+                        catch (Exception)
+                        {
+                            return new DataTable();
+                        }
+                    }
+                }
+            });
+        }
+
+        public async Task<object> ExecuteScalar(string query, params Parameter[] parameters)
+        {
+            return await Task.Run(() =>
+            {
+                using (Connection conn = (Connection)Activator.CreateInstance(typeof(Connection), connectionString.ToString()))
+                {
+                    conn.Open();
+
+                    using (Command cmd = (Command)Activator.CreateInstance(typeof(Command), query, conn))
+                    {
+                        foreach (var param in parameters)
+                            cmd.Parameters.Add(param);
+
+                        try
+                        {
+                            object returnVal = cmd.ExecuteScalar();
+                            conn.Close();
+                            return returnVal;
+                        }
+                        catch (Exception)
+                        {
+                            return null;
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Spell-Editor/WorldDatabase.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Spell_Editor
+{
+    class WorldDatabase : Database<MySqlConnection, MySqlConnectionStringBuilder, MySqlParameter, MySqlCommand, MySqlTransaction>
+    {
+        public WorldDatabase(string host, uint port, string username, string password, string databaseName)
+        {
+            connectionString = new MySqlConnectionStringBuilder();
+            connectionString.Server = host;
+            connectionString.Port = (uint)port;
+            connectionString.UserID = username;
+            connectionString.Password = password;
+            connectionString.Database = databaseName;
+            connectionString.AllowUserVariables = true;
+            connectionString.AllowZeroDateTime = true;
+        }
+
+        public async Task<int> GetCreatureIdByGuid(int guid)
+        {
+            DataTable dt = await ExecuteQuery("SELECT id FROM creature WHERE guid = '" + guid + "'");
+
+            if (dt.Rows.Count == 0)
+                return 0;
+
+            return Convert.ToInt32(dt.Rows[0]["id"]);
+        }
+
+        public async Task<int> GetGameobjectIdByGuid(int guid)
+        {
+            DataTable dt = await ExecuteQuery("SELECT id FROM gameobject WHERE guid = '" + guid + "'");
+
+            if (dt.Rows.Count == 0)
+                return 0;
+
+            return Convert.ToInt32(dt.Rows[0]["id"]);
+        }
+
+        public async Task<string> GetCreatureNameById(int id)
+        {
+            DataTable dt = await ExecuteQuery("SELECT name FROM creature_template WHERE entry = '" + id + "'");
+
+            if (dt.Rows.Count == 0)
+                return "_replaceBecauseOfError_ GetCreatureNameById not found";
+
+            string name = dt.Rows[0]["name"].ToString();
+            return name.Replace('"', '\'');
+        }
+
+        public async Task<string> GetCreatureNameByGuid(int guid)
+        {
+            DataTable dt = await ExecuteQuery("SELECT name FROM creature_template WHERE entry = '" + await GetCreatureIdByGuid(guid) + "'");
+
+            if (dt.Rows.Count == 0)
+                return "_replaceBecauseOfError_ GetCreatureNameByGuid not found";
+
+            string name = dt.Rows[0]["name"].ToString();
+            return name.Replace('"', '\'');
+        }
+
+        public async Task<string> GetGameobjectNameById(int id)
+        {
+            DataTable dt = await ExecuteQuery("SELECT name FROM gameobject_template WHERE entry = '" + id + "'");
+
+            if (dt.Rows.Count == 0)
+                return "_replaceBecauseOfError_ GetGameobjectNameById not found";
+
+            string name = dt.Rows[0]["name"].ToString();
+            return name.Replace('"', '\'');
+        }
+
+        public async Task<string> GetGameobjectNameByGuid(int guid)
+        {
+            DataTable dt = await ExecuteQuery("SELECT name FROM gameobject_template WHERE entry = '" + await GetGameobjectIdByGuid(guid) + "'");
+
+            if (dt.Rows.Count == 0)
+                return "_replaceBecauseOfError_ GetGameobjectNameByGuid not found";
+
+            string name = dt.Rows[0]["name"].ToString();
+            return name.Replace('"', '\'');
+        }
+
+        public async Task<string> GetQuestNameById(int id)
+        {
+            DataTable dt = await ExecuteQuery("SELECT title FROM quest_template WHERE id = " + id);
+
+            if (dt.Rows.Count == 0)
+                return "_replaceBecauseOfError_<Quest '" + id + "' not found!>";
+
+            return dt.Rows[0]["title"].ToString();
+        }
+
+        public async Task<string> GetQuestNameForCastedByCreatureOrGo(int requiredNpcOrGo1, int requiredNpcOrGo2, int requiredNpcOrGo3, int requiredNpcOrGo4, int requiredSpellCast1)
+        {
+            DataTable dt = await ExecuteQuery(String.Format("SELECT title FROM quest_template WHERE (RequiredNpcOrGo1 = {0} OR RequiredNpcOrGo2 = {1} OR RequiredNpcOrGo3 = {2} OR RequiredNpcOrGo4 = {3}) AND RequiredSpellCast1 = {4}", requiredNpcOrGo1, requiredNpcOrGo2, requiredNpcOrGo3, requiredNpcOrGo4, requiredSpellCast1));
+
+            if (dt.Rows.Count == 0)
+                return "_replaceBecauseOfError_<Quest not found (GetQuestNameForCastedByCreatureOrGo)!>";
+
+            return dt.Rows[0]["title"].ToString();
+        }
+
+        public async Task<string> GetQuestNameForKilledMonster(int requiredNpcOrGo1, int requiredNpcOrGo2, int requiredNpcOrGo3, int requiredNpcOrGo4)
+        {
+            DataTable dt = await ExecuteQuery(String.Format("SELECT title FROM quest_template WHERE (RequiredNpcOrGo1 = {0} OR RequiredNpcOrGo2 = {1} OR RequiredNpcOrGo3 = {2} OR RequiredNpcOrGo4 = {3})", requiredNpcOrGo1, requiredNpcOrGo2, requiredNpcOrGo3, requiredNpcOrGo4));
+
+            if (dt.Rows.Count == 0)
+                return "_replaceBecauseOfError_<Quest not found (GetQuestNameForKilledMonster)!>";
+
+            return dt.Rows[0]["title"].ToString();
+        }
+
+        public async Task<string> GetItemNameById(int id)
+        {
+            DataTable dt = await ExecuteQuery("SELECT name FROM item_template WHERE entry = " + id);
+
+            if (dt.Rows.Count == 0)
+                return "_replaceBecauseOfError_<Item '" + id + "' not found!>";
+
+            return dt.Rows[0]["name"].ToString();
+        }
+    }
+}
